6314e825f2b5bf42e370746bf07e2d6ece5b547e
Lucas Carvalho <lucas.carvalho200608@gmail.com>
2026-03-12 15:49:13 -0300

    "Correção na validação de CPF"

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 
 public  class Usuario
 {
    public string Nome { get; set; }
+   public string Cpf { get; set; }
    public string Email { get; set; }
    public int  Idade { get; set; }
 }
@@ -15,6 +16,23 @@ class Program
 
         Console.WriteLine("Digite o Nome do usuário:");
         string nome = Console.ReadLine();
+
+        string cpf;
+
+        // LOOP PARA VALIDAR CPF
+        while (true)
+        {
+            Console.WriteLine("Digite o CPF do usuário (apenas números):");
+            cpf = Console.ReadLine();
+
+            // verifica se tem 11 números e se todos são dígitos
+            if (cpf.Length == 11 && long.TryParse(cpf, out _))
+            {
+                break; // CPF válido
+            }
+
+            Console.WriteLine("CPF inválido. O CPF deve conter exatamente 11 números.");
+        }
 
         string email;
 
@@ -55,17 +73,23 @@ class Program
                 continue;
             }
 
-            break; // idade válida
+            break; // idade 
 
         }
             Usuario usuario = new Usuario
             {
                 Nome = nome,
+                Cpf = cpf,
                 Email = email,
                 Idade = idade
             };
 
-               Console.WriteLine("Usuário cadastrado com sucesso!");
+        Console.WriteLine("Usuário cadastrado com sucesso!");
+        Console.WriteLine("\nUsuário cadastrado com sucesso!");
+        Console.WriteLine("Nome: " + usuario.Nome);
+        Console.WriteLine("CPF: " + usuario.Cpf);
+        Console.WriteLine("Email: " + usuario.Email);
+        Console.WriteLine("Idade: " + usuario.Idade);
 
 
 
